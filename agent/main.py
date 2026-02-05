9847937cd0af18fa15b9213c2fee8812f474b074
timhealey3 <timhealey34@gmail.com>
2026-02-04 22:08:59 -0500

    add agent

---
+++ agent/main.py
@@ -0,0 +1,7 @@
+
+# workflow
+# run once an hour with cronjob
+# agent reviews data collected in the past hour 
+# agent writes report summarizing data
+# if there is something unusual it creates an incident report as well
+
